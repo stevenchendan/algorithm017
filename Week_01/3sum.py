d9050834285f95d0e0eb4aa088bb9aca97850ca6
stevenchendan <qwe16165850@gmail.com>
2020-10-08 21:03:32 +1100

    Create 3sum.py

---
+++ Week_01/3sum.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/3sum/submissions/
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        if not nums or len(nums) < 3:
+            return []
+        # two loop
+        result = []
+        nums.sort()
+        for i in range(len(nums) - 2):
+            # 
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
+                else:
+                    result.append((nums[i], nums[l], nums[r]))
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+        return result
